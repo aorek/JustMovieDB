8c2c947ace5191587884f4568812cb91379c48c1
aorek <aquerorodriguez@gmail.com>
2020-11-03 10:10:21 +0100

    Add routes

---
+++ src/app/app-router.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { FilmComponent } from './pages/film/film.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'search/:text',
+    component: SearchComponent,
+  },
+  {
+    path: 'film/:id',
+    component: FilmComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+  },
+];
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRouterModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AppRouterModule } from './app-router.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, AppRouterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
