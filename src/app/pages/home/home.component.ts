64da525d236a71eeb6f01959e97c57ce3b40ce48
aorek <aquerorodriguez@gmail.com>
2020-11-04 01:18:58 +0100

    Create scroll pagination

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/nowPlaying-response';
 import { FilmsService } from 'src/app/services/films.service';
 
@@ -8,15 +8,31 @@ import { FilmsService } from 'src/app/services/films.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  public moviesSlice: Movie[];
   public movies: Movie[];
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    const scrollPos =
+      (document.documentElement.scrollTop || document.body.scrollTop) + 1300;
+    const scrollHeight =
+      document.documentElement.scrollHeight || document.body.scrollHeight;
+
+    if (scrollPos > scrollHeight) {
+      this.filmService.getNowPlaying().subscribe((movies) => {
+        this.movies.push(...movies);
+      });
+    }
+  }
 
   constructor(private filmService: FilmsService) {
     this.movies = [];
   }
 
   ngOnInit(): void {
-    this.filmService
-      .getNowPlaying()
-      .subscribe(({ results }) => (this.movies = results));
+    this.filmService.getNowPlaying().subscribe((movies) => {
+      this.movies = movies;
+      this.moviesSlice = movies;
+    });
   }
 }
--- src/app/services/films.service.ts
+++ src/app/services/films.service.ts
@@ -1,18 +1,45 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
-import { NowPlaying } from '../interfaces/nowPlaying-response';
+import { Movie, NowPlaying } from '../interfaces/nowPlaying-response';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FilmsService {
-  constructor(private http: HttpClient) {}
+  private apiUrl: string = 'https://api.themoviedb.org/3';
+  public page: number;
+  public loading: boolean;
 
-  getNowPlaying(): Observable<NowPlaying> {
-    return this.http.get<NowPlaying>(
-      'https://api.themoviedb.org/3/movie/now_playing?api_key=214c473020b1ecaef118c494921c7f6c&language=es-ES&page=1'
-    );
+  constructor(private http: HttpClient) {
+    this.page = 1;
+    this.loading = false;
+  }
+
+  get params() {
+    return {
+      api_key: '214c473020b1ecaef118c494921c7f6c',
+      language: 'es-ES',
+      page: this.page.toString(),
+    };
+  }
+
+  getNowPlaying(): Observable<Movie[]> {
+    if (this.loading) return of([]);
+
+    this.loading = true;
+    return this.http
+      .get<NowPlaying>(`${this.apiUrl}/movie/now_playing`, {
+        params: this.params,
+      })
+      .pipe(
+        map(({ results }) => results),
+        tap(() => {
+          this.page += 1;
+          this.loading = false;
+        })
+      );
   }
 }
